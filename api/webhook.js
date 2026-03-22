fac99b6167d003c2da3f66b48221c989d457353b
luigi crescenzo <luicrescenzo@gmail.com>
2026-03-22 13:17:58 +0100

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -78,7 +78,9 @@ export default async function handler(req, res) {
 
   console.log(`Webhook ricevuto: ${eventName}`);
 
-  try {
+        res.status(200).json({ ok: true });
+
+        try {
     switch (eventName) {
       case "order_created":
         await handleOrderCreated(data);
